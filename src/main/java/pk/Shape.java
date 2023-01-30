54851bb1cb7d51877f33cc2e691c9b21337a61ef
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-01-30 15:28:35 +0530

    Update Shape.java

--- src/main/java/pk/Shape.java
+++ src/main/java/pk/Shape.java
@@ -74,43 +74,50 @@ public class Shape {
     public static void main(String[] args){
         int n = 1;
         do{
-            LOGGER.log(Level.INFO,"\n1.Triangle 2.Rectangle 3.Circle\n");
-            int choice = sc.nextInt();
-            if(choice == 1){
-                LOGGER.log(Level.INFO,"\nenter side 1: ");
-                double side1 = sc.nextDouble();
-                LOGGER.log(Level.INFO,"\nenter side 2: ");
-                double side2 = sc.nextDouble();
-                LOGGER.log(Level.INFO,"\nenter base: ");
-                double base = sc.nextDouble();
-                LOGGER.log(Level.INFO,"\nenter height: ");
-                double height = sc.nextDouble();
+            try{
+                LOGGER.log(Level.INFO,"\n1.Triangle 2.Rectangle 3.Circle\n");
+                int choice = sc.nextInt();
+                if(choice == 1){
+                    LOGGER.log(Level.INFO,"\nenter side 1: ");
+                    double side1 = sc.nextDouble();
+                    LOGGER.log(Level.INFO,"\nenter side 2: ");
+                    double side2 = sc.nextDouble();
+                    LOGGER.log(Level.INFO,"\nenter base: ");
+                    double base = sc.nextDouble();
+                    LOGGER.log(Level.INFO,"\nenter height: ");
+                    double height = sc.nextDouble();
 
-                Shapes s = new Triangle(side1,side2,base,height);
-                s.perimeter();
-                s.area();
-            }
-            else if(choice == 2){
-                LOGGER.log(Level.INFO,"enter length: ");
-                double length = sc.nextDouble();
-                LOGGER.log(Level.INFO,"enter breadth: ");
-                double breadth = sc.nextDouble();
+                    Shapes s = new Triangle(side1,side2,base,height);
+                    s.perimeter();
+                    s.area();
+                }
+                else if(choice == 2){
+                    LOGGER.log(Level.INFO,"enter length: ");
+                    double length = sc.nextDouble();
+                    LOGGER.log(Level.INFO,"enter breadth: ");
+                    double breadth = sc.nextDouble();
 
-                Shapes s = new Rectangle(length, breadth);
-                s.perimeter();
-                s.area();
-            }
-            else if(choice == 3){
-                LOGGER.log(Level.INFO,"enter radius: ");
-                double radius = sc.nextDouble();
+                    Shapes s = new Rectangle(length, breadth);
+                    s.perimeter();
+                    s.area();
+                }
+                else if(choice == 3){
+                    LOGGER.log(Level.INFO,"enter radius: ");
+                    double radius = sc.nextDouble();
 
-                Shapes s = new Circle(radius);
-                s.perimeter();
-                s.area();
-            }
-            else{
-                LOGGER.log(Level.INFO,"exiting..");
-                n=0;
+                    Shapes s = new Circle(radius);
+                    s.perimeter();
+                    s.area();
+                }
+                else{
+                    LOGGER.log(Level.INFO,"exiting..");
+                    n=0;
+                }
+            }catch(Exception e){
+                String s = ""+e;
+                LOGGER.info(s);
+                sc.nextLine();
+                LOGGER.info("_______________________________________________");
             }
         }while(n == 1);
     }
